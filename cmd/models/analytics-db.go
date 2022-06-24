8ef6776d8c06648c8bceaa97d40ad53dfd682be9
Bryan <ho.cheng.en.bryan@gmail.com>
2022-06-24 15:02:44 +0800

    making analytics api faster

--- cmd/models/analytics-db.go
+++ cmd/models/analytics-db.go
@@ -6,92 +6,94 @@ import (
 	"time"
 )
 
-//todo figure out if there is a faster way to do the queries, now taking more
-//than 3 sec which is too long for an api
 func GetAnalytics(db *sql.DB, userId, startDate, endDate string) (*AnalyticsBreakdown, error) {
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 	defer cancel()
 
-	assignmentQuery := `SELECT COUNT(*) FROM alltasks
+	query := `SELECT COUNT(id) FROM alltasks
 				WHERE userId = ? 
-				AND dailyLogDate >= ? 
+				AND dailyLogDate >= ?
 				AND dailyLogdate <= ?
-				AND taskType = 0;`
-	assignmentRow := db.QueryRowContext(ctx, assignmentQuery, userId, startDate, endDate)
-	var assignments int
-	err := assignmentRow.Scan(&assignments)
-	if err != nil {
-		return nil, err
-	}
-
-	eventQuery := `SELECT COUNT(*) FROM alltasks
-				WHERE userId = ? 
-				AND dailyLogDate >= ? 
+				AND taskType = 0
+				UNION
+				SELECT COUNT(id) FROM alltasks
+				WHERE userId = ?
+				AND dailyLogDate >= ?
 				AND dailyLogdate <= ?
-				AND taskType = 1;`
-	eventRow := db.QueryRowContext(ctx, eventQuery, userId, startDate, endDate)
-	var events int
-	err = eventRow.Scan(&events)
-	if err != nil {
-		return nil, err
-	}
-
-	noteQuery := `SELECT COUNT(*) FROM alltasks
+				AND taskType = 1
+				UNION 
+				SELECT COUNT(id) FROM alltasks
 				WHERE userId = ? 
-				AND dailyLogDate >= ? 
+				AND dailyLogDate >= ?
 				AND dailyLogdate <= ?
-				AND taskType = 2;`
-	noteRow := db.QueryRowContext(ctx, noteQuery, userId, startDate, endDate)
-	var notes int
-	err = noteRow.Scan(&notes)
-	if err != nil {
-		return nil, err
-	}
-
-	effortQuery := `SELECT SUM(effort) FROM alltasks
+				AND taskType = 2
+				UNION
+				SELECT SUM(effort) FROM alltasks
 				WHERE userId = ? 
-				AND dailyLogDate >= ? 
-				AND dailyLogdate <= ?;`
-	effortRow := db.QueryRowContext(ctx, effortQuery, userId, startDate, endDate)
-	var effort int
-	err = effortRow.Scan(&effort)
-	if err != nil {
-		return nil, err
-	}
-
-	completedEventsQuery := `SELECT COUNT(*) FROM alltasks
-				WHERE userId = ? 
-				AND dailyLogDate >= ? 
+				AND dailyLogDate >= ?
+				AND dailyLogdate <= ?
+				UNION
+				SELECT COUNT(id) FROM alltasks
+				WHERE userId = ?
+				AND dailyLogDate >= ?
 				AND dailyLogdate <= ?
 				AND isCompleted = 0
-				AND (taskType = 0 OR taskType = 1);`
-	completedEventsRow := db.QueryRowContext(ctx, completedEventsQuery, userId, startDate, endDate)
-	var completedEvents int
-	err = completedEventsRow.Scan(&completedEvents)
-	if err != nil {
-		return nil, err
-	}
-
-	completedEffortQuery := `SELECT SUM(effort) FROM alltasks
-				WHERE userId = ? 
-				AND dailyLogDate >= ? 
+				AND (taskType = 0 OR taskType = 1)
+				UNION
+				SELECT SUM(effort) FROM alltasks
+				WHERE userId = ?
+				AND dailyLogDate >= ?
 				AND dailyLogdate <= ?
-				AND isCompleted = 0;`
-	completedEffortRow := db.QueryRowContext(ctx, completedEffortQuery, userId, startDate, endDate)
-	var completedEffort int
-	err = completedEffortRow.Scan(&completedEffort)
+				AND isCompleted = 0`
+
+
+	//fix this
+	rows, err := db.QueryContext(ctx, query, 
+					userId, startDate, endDate,
+					userId, startDate, endDate,
+					userId, startDate, endDate,
+					userId, startDate, endDate,
+					userId, startDate, endDate,
+					userId, startDate, endDate)
 	if err != nil {
 		return nil, err
 	}
+	defer rows.Close()
 
-	breakdown := AnalyticsBreakdown{
-		Assignments: assignments,
-		Events: events,
-		Notes: notes,
-		TotalEffort: effort,
-		TotalCompletedEvents: completedEvents,
-		TotalCompletedEffort: completedEffort,
+	count := 0
+	var breakdown AnalyticsBreakdown
+	var scanErr error
+	for rows.Next() {
+		if count == 0 {
+			scanErr = rows.Scan(
+				&breakdown.Assignments,
+			)	
+		} else if count == 1 {
+			scanErr = rows.Scan(
+				&breakdown.Events,
+			)
+		} else if count == 2 {
+			scanErr = rows.Scan(
+				&breakdown.Notes,
+			)
+		} else if count == 3 {
+			scanErr = rows.Scan(
+				&breakdown.TotalEffort,
+			)
+		} else if count == 4 {
+			scanErr = rows.Scan(
+				&breakdown.TotalCompletedEvents,
+			)
+		} else if count == 5 {
+			scanErr = rows.Scan(
+				&breakdown.TotalCompletedEffort,
+			)
+		}
+		if scanErr != nil {
+			return nil, err
+		}
+		count++
 	}
-
+	
 	return &breakdown, nil
 }
